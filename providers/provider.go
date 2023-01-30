f37666eb7c99ad7e6c4bbcace592fa57c5b18fd3
yanghao05 <yanghao05@qianxin.com>
2023-01-30 10:49:26 +0800

    add faker provider

--- providers/provider.go
+++ providers/provider.go
@@ -3,6 +3,7 @@ package providers
 import (
 	_ "atom/providers/config"
 	_ "atom/providers/database"
+	_ "atom/providers/faker"
 	_ "atom/providers/http"
 	_ "atom/providers/logger"
 	_ "atom/providers/micro"
