830064219ffff4c5c0b93116ac3837fef033f937
yanghao05 <yanghao05@qianxin.com>
2023-06-16 08:42:09 +0800

    remove ds

--- ds/request/page.go
+++
@@ -1,6 +0,0 @@
-package request
-
-type PageFilter struct {
-	Page    uint64 `form:"page"`
-	PerPage uint64 `form:"per_page"`
-}
--- ds/response/page.go
+++
@@ -1,6 +0,0 @@
-package response
-
-type PageResponse[T any] struct {
-	Items []T    `json:"items,omitempty"`
-	Total uint64 `json:"total,omitempty"`
-}
